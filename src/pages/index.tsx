d193fdc8c0d55c7330a28952a4c9cea3be99388b
Banquez09 <maria.banquez1@soysena.edu.co>
2025-08-11 23:53:02 -0500

    Endpoint login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,9 +19,9 @@ export default function Home() {
     setError("")
 
     try {
-      const response = await fetch("http://localhost:3001/auth/login", {
+     // const response = await fetch("http://localhost:3001/auth/login", {
 
-     //   const response = await fetch("https://backendamed-production.up.railway.app/auth/login", {
+        const response = await fetch("https://backendamed-production.up.railway.app/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
